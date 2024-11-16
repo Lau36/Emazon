63fa3297767c970dc32af2b4dd9c540a0a4b9426
Lau36 <laura.jaimes@correounivalle.edu.co>
2024-11-15 20:33:47 -0500

    styles in create category, brand and item form improved

--- src/app/utils/customer-header.ts
+++ src/app/utils/customer-header.ts
@@ -1,5 +1,7 @@
 export const listNavCustomerHeader: {elementName: string, path:string, icon?: string}[] =
 [
+  {elementName: "Categorías", path:"cliente/categorias"},
+  {elementName: "Marcas", path:"cliente/marcas"},
   {elementName: "Artículos", path:"cliente/articulos"},
   {elementName: "Carrito", path:"cliente/carrito", icon: "../../assets/cartNav.svg"},
 ]
